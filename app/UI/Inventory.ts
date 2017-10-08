ffbd88157053c363092af843c6294824e81ecbc6
Manojlović Miloš <manojlovicsmilos@hotmail.com>
2017-10-08 03:40:17 +0200

    Inventory - 2

--- app/UI/Inventory.ts
+++ app/UI/Inventory.ts
@@ -16,10 +16,15 @@ class Inventory extends Window
         this.CreateBorder();
         this.AddElement(new Engineer.Math.Vertex(200,200,2.5), new Engineer.Math.Vertex(300,460,1), 1, Engineer.Math.Color.FromRGBA(60,60,60,255));
         this.AddElement(new Engineer.Math.Vertex(220,80,2.5), new Engineer.Math.Vertex(50,50,1), 0);
+        this.AddElement(new Engineer.Math.Vertex(220,80,2.5), new Engineer.Math.Vertex(50,50,1), 2, Engineer.Math.Color.FromRGBA(30,30,30,255));
         this.AddElement(new Engineer.Math.Vertex(120,170,2.5), new Engineer.Math.Vertex(50,50,1), 0);
+        this.AddElement(new Engineer.Math.Vertex(120,170,2.5), new Engineer.Math.Vertex(50,50,1), 4, Engineer.Math.Color.FromRGBA(30,30,30,255));
         this.AddElement(new Engineer.Math.Vertex(320,170,2.5), new Engineer.Math.Vertex(50,50,1), 0);
+        this.AddElement(new Engineer.Math.Vertex(320,170,2.5), new Engineer.Math.Vertex(50,50,1), 5, Engineer.Math.Color.FromRGBA(30,30,30,255));
         this.AddElement(new Engineer.Math.Vertex(220,170,2.5), new Engineer.Math.Vertex(50,50,1), 0);
+        this.AddElement(new Engineer.Math.Vertex(220,170,2.5), new Engineer.Math.Vertex(50,50,1), 3, Engineer.Math.Color.FromRGBA(30,30,30,255));
         this.AddElement(new Engineer.Math.Vertex(220,310,2.5), new Engineer.Math.Vertex(50,50,1), 0);
+        this.AddElement(new Engineer.Math.Vertex(220,310,2.5), new Engineer.Math.Vertex(50,50,1), 2, Engineer.Math.Color.FromRGBA(30,30,30,255));
         for(let i = 0; i < 5; i++)
         {
             for(let j = 0; j < 9; j++)
--- app/UI/Window.ts
+++ app/UI/Window.ts
@@ -22,7 +22,7 @@ class Window extends Engineer.Engine.Tile
         this._Elements = [];
         this._Decorations = [];
         this._DecorationT = new Engineer.Engine.TileCollection(null, ["/build/resources/border_c.png","/build/resources/border_h.png","/build/resources/border_v.png"]);
-        this._ElementT = new Engineer.Engine.TileCollection(null, ["/build/resources/inv_grid_c.png","/build/resources/inv_vorlok.png"]);
+        this._ElementT = new Engineer.Engine.TileCollection(null, ["/build/resources/elements/grid.png","/build/resources/elements/vorlok.png","/build/resources/elements/helm.png","/build/resources/elements/armor.png","/build/resources/elements/wand.png","/build/resources/elements/tome.png"]);
     }
     public Init() : void
     {
