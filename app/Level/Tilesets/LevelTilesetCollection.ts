c934b65b287cec3bdeafa925fe83a9c8e3e94803
Manojlović Miloš <manojlovicsmilos@hotmail.com>
2017-11-01 19:41:01 +0100

    Town Tileset Update

--- app/Level/Tilesets/LevelTilesetCollection.ts
+++ app/Level/Tilesets/LevelTilesetCollection.ts
@@ -11,7 +11,7 @@ class LevelTilesetCollection
         this.Items = {};
         this.Items["Ruin"] = new LevelTileset("Ruin", LevelTilesetLayoutType.Bordered, LevelTilesetFloorType.Uniform, LevelTilesetWallType.Uniform, LevelTilesetCeilingType.Bordered, [16,1]);
         this.Items["Cathedral"] = new LevelTileset("Cathedral", LevelTilesetLayoutType.Story, LevelTilesetFloorType.Checkered, LevelTilesetWallType.Uniform, LevelTilesetCeilingType.Crested, [4,8]);
-        this.Items["Town"] = new LevelTileset("Town", LevelTilesetLayoutType.Bordered, LevelTilesetFloorType.Uniform, LevelTilesetWallType.Bordered, LevelTilesetCeilingType.Roofed, [1,7]);
+        this.Items["Town"] = new LevelTileset("Town", LevelTilesetLayoutType.Bordered, LevelTilesetFloorType.Uniform, LevelTilesetWallType.Bordered, LevelTilesetCeilingType.Roofed, [3,7]);
         //this.Items["Beach"] = new LevelTileset("Beach", LevelTilesetLayoutType.Story, LevelTilesetFloorType.Uniform, LevelTilesetWallType.Uniform, LevelTilesetCeilingType.Uniform, [5,1]);
     }
     public static Single:LevelTilesetCollection;
